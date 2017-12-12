13ed53cefe77d4e89b1a38b1beb219c411738796
Programmer Muslim <rizki@imastudio.co.id>
2017-12-12 23:28:27 +0700

    add

--- app/src/main/java/imastudio/id/co/loginregretrofit/MenuUtama/MainMenu.java
+++ app/src/main/java/imastudio/id/co/loginregretrofit/MenuUtama/MainMenu.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import imastudio.id.co.loginregretrofit.R;
+
 public class MainMenu extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
